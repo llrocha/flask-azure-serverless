5a3f78c0ab3b4e6ed91a12a8bbc084890b899112
Leonardo Larsen Rocha <brhbs0305@ambev.com.br>
2021-01-29 16:38:25 -0300

    Fixing missed var

--- app.py
+++ app.py
@@ -21,7 +21,8 @@ def scale(payload):
 @app.route("/")
 def home():
     html = "<h3>Sklearn Prediction Home</h3>"
-    return html.format(format)
+    #return html.format(format)
+    return html.format()
 
 # TO DO:  Log out the prediction value
 @app.route("/predict", methods=['POST'])
